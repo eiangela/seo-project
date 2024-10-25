3126f22641c3574b556ebaf9478fdbcfd03686d3
Angela R <angelaribeiro2@hotmail.com>
2024-10-25 11:55:39 -0300

    WIP

---
+++ src/components/OurConsulting.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="6">
+        <v-card max-width="500" class="pa-5 our-consulting-card rounded-xl">
+          <img
+            src="@/assets/images/portrait-happy-bearded-young-man-glasses-standing-outdoors_171337-14763.jpg"
+            alt="Rapaz sorrindo"
+            class="our-consulting-image"
+          />
+          <v-card-title class="our-consulting-card-title"
+            >I felt like I was a good designer</v-card-title
+          >
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="6" md="6">
+        <h3
+          class="our-consulting-subtitle font-weight-regular text-uppercase mb-5"
+        >
+          Sample Headline
+        </h3>
+        <h2 class="our-consulting-title mb-5">
+          Our management consulting services focus on our clients' most critical
+          issues
+        </h2>
+        <p class="our-consulting-paragraph mb-10 font-weight-bold">
+          I thought I had it figured out – I'd moved from Gujarat to Texas and
+          was working with a trailblazing team at AT&T, literally one of the
+          biggest companies in the world.
+        </p>
+        <p class="our-consulting-paragraph mb-5">
+          Sem fringilla ut morbi tincidunt augue interdum velit. Nunc sed
+          blandit libero volutpat sed cras. Risus quis varius quam quisque id.
+          Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. Et
+          tortor consequat id porta nibh. Tincidunt id aliquet risus feugiat in
+          ante metus dictum at. Feugiat pretium nibh ipsum consequat.
+        </p>
+        <v-btn class="our-consulting-btn font-weight-bold" width="200" height="50">read more</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/pages/HomeIndex.vue
+++ src/pages/HomeIndex.vue
@@ -1,13 +1,18 @@
 <script setup>
 import SeoIntro from "../components/SeoIntro.vue";
+import OurConsulting from "../components/OurConsulting.vue";
 </script>
 
 <template>
-    <section class="seo-intro">
-        <div class="background-image"></div>
-        
-        <div class="content-wrapper">
-          <SeoIntro />
-        </div>
-      </section>
+  <section class="seo-intro">
+    <div class="background-image"></div>
+
+    <div class="content-wrapper">
+      <SeoIntro />
+    </div>
+  </section>
+
+  <section>
+    <OurConsulting />
+  </section>
 </template>
